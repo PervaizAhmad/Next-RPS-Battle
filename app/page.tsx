3c0052451afab32042db9df81bb9b2fcc0e4153c
PervaizAhmad <pervaiz.ahmad1999@gmail.com>
2023-10-06 23:22:51 +0100

    feat: add game logic

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@
 import Moves from "@/components/moves";
 import Results from "@/components/results";
 import ScoreBoard from "@/components/scoreBoard";
-import { useState } from "react";
+import { useState, useEffect, useRef } from "react";
 
 export enum GameMoves {
   ROCK = 0,
@@ -12,13 +12,61 @@ export enum GameMoves {
 }
 
 export default function Home() {
-  const [showResults, setShowResults] = useState(true);
+  const [showResults, setShowResults] = useState(false);
   const [playerScore, setPlayerScore] = useState(0);
   const [computerScore, setComputerScore] = useState(0);
-  
+  const [moves, setMoves] = useState<{
+    player: GameMoves;
+    computer: GameMoves;
+    win: boolean;
+  }>({ player: GameMoves.ROCK, computer: GameMoves.ROCK, win: false });
+
+  const [isFirstMount, setIsFirstMount] = useState(true); // ref to store first mount flag
+
+  useEffect(() => {
+    if (isFirstMount) {
+      // skip if component is mounted for the first time
+      setIsFirstMount(false);
+    } else {
+      setShowResults(true);
+    }
+  }, [moves]);
+
   const resetScore = (): void => {
     setPlayerScore(0);
     setComputerScore(0);
+  };
+
+  const calculateResults = (playerMove: GameMoves): void => {
+    // generate a random move
+    const computerMove: GameMoves = Math.floor(Math.random() * 3);
+
+    // check if player won
+    let win = false;
+    switch (playerMove) {
+      case 0:
+        if (computerMove === 2) win = true;
+        break;
+      case 1:
+        if (computerMove === 0) win = true;
+        break;
+      case 2:
+        if (computerMove === 1) win = true;
+        break;
+    }
+
+    if (win) {
+      console.warn("win");
+      setPlayerScore(playerScore + 1);
+    } else if (playerMove !== computerMove) {
+      console.warn("loss");
+      setComputerScore(computerScore + 1);
+    } else {
+      console.warn("draw");
+    }
+
+    // update state --> useEffect will be triggered
+    setMoves({ player: playerMove, computer: computerMove, win });
   };
 
   return (
@@ -33,15 +81,15 @@ export default function Home() {
       {showResults ? (
         <Results
           moves={{
-            player: GameMoves.ROCK,
-            computer: GameMoves.ROCK,
+            player: moves.player,
+            computer: moves.computer,
           }}
-          win={false}
-          draw={true}
+          win={moves.win}
+          draw={moves.player === moves.computer}
           playAgain={() => setShowResults(false)}
         />
       ) : (
-        <Moves />
+        <Moves makeMove={calculateResults} />
       )}
     </main>
   );
--- components/moves.tsx
+++ components/moves.tsx
@@ -1,12 +1,16 @@
 import { FaHandPaper, FaHandRock, FaHandScissors } from "react-icons/fa";
 import { GameMoves } from "@/app/page";
 
-export default function Moves() {
+export default function Moves({
+  makeMove,
+}: {
+  makeMove: (sign: GameMoves) => void;
+}) {
   return (
     <div className="flex justify-center items-center flex-col w-full">
       <h1 className="text-2xl font-bold">MAKE YOUR MOVE</h1>
       <div className="grid grid-cols-3 pt-10">
-        <div className="grid place-items-center col-start-2 rounded-full bg-slate-200 cursor-pointer hover:bg-blue-300 ease-out duration-300">
+        <div className="grid place-items-center col-start-2 rounded-full bg-slate-200 cursor-pointer hover:bg-blue-300 ease-out duration-300" onClick={() => makeMove(GameMoves.SCISSOR)}>
           <div className="grid place-items-center w-[150px] rounded-full aspect-square border-[10px] border-blue-500">
             <FaHandScissors size="4rem" color="#1F3556" />
           </div>
@@ -16,13 +20,13 @@ export default function Moves() {
           <div className="w-[100px] aspect-square">{/* Empty Spacer */}</div>
         </div>
 
-        <div className="grid place-items-center col-start-1 rounded-full bg-slate-200 cursor-pointer hover:bg-red-300 ease-out duration-300">
+        <div className="grid place-items-center col-start-1 rounded-full bg-slate-200 cursor-pointer hover:bg-red-300 ease-out duration-300" onClick={() => makeMove(GameMoves.ROCK)}>
           <div className="grid place-items-center w-[150px] rounded-full aspect-square border-[10px] border-red-500">
             <FaHandRock size="4rem" color="#1F3556" />
           </div>
         </div>
 
-        <div className="grid place-items-center col-start-3 rounded-full bg-slate-200 cursor-pointer hover:bg-green-300 ease-out duration-300">
+        <div className="grid place-items-center col-start-3 rounded-full bg-slate-200 cursor-pointer hover:bg-green-300 ease-out duration-300" onClick={() => makeMove(GameMoves.PAPER)}>
           <div className="grid place-items-center w-[150px] rounded-full aspect-square border-[10px] border-green-500">
             <FaHandPaper size="4rem" color="#1F3556" />
           </div>
